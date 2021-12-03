125e15ecc6fb6fcedcd5e3d84f61de4aadca9354
anandasatriaadi <putu.satriaadi@gmail.com>
2021-12-03 23:00:27 +0800

    feat: add select2, real province data on checkout

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -1,62 +1,70 @@
 <x-layout titlePage="Hanaka | Cart">
-  <div class="md:mx-8">
-    <x-navbar></x-navbar>
-  
-    {{-- TAMBAHIN INPUT PER ITEM, JADI BISA BELI LEBIH DARI 1 BARANG SEKALIGUS --}}
-    <h2 class="text-3xl font-bold text-gray-900 text-center mt-8">Keranjang</h2>
-    <div class="grid grid-cols-8 gap-x-8 mx-8 mt-4">
-      {{-- card section  --}}
-      <div class="col-span-8 lg:col-span-5 flex flex-col">
-        <div class="flex flex-col space-y-8">
-          @for ($i = 0; $i < 3; $i++)
-          <div class="flex content-center items-center space-x-4 rounded-lg shadow-custom1 p-4">
-            <input type="checkbox" name="" id="">
-            <div class="cart-item-image-container rounded-lg" style="background-image: url({{ asset("images/IMG_7800.jpg") }}); background-size: cover; backround-repeat: none">
-            </div>
-            <div class="flex flex-col flex-grow h-full">
-              <div>
-                <h4 class="text-lg font-semibold">Baju baru ni</h4>
-                <p class="">Rp. 2.000.000,00</p>
-              </div>
-              <div class="flex justify-end items-center mt-auto">
-                <p class="text-gray-400 hover:text-blue-500 cursor-pointer">Pindahkan ke Wishlist</p>
-                <p class="text-gray-400">&nbsp;|&nbsp;</p>
-                <div class="flex flex-col align-center"><i class='text-2xl text-gray-400 hover:text-blue-500 cursor-pointer bx bx-trash'></i></div>
-                <p class="text-gray-400">&nbsp;|&nbsp;</p>
-                <div class="flex space-x-2">
-                  <i class='text-2xl text-gray-400 hover:text-blue-500 cursor-pointer bx bx-minus'></i>
-                  {{-- <input class="flex-shrink" type="number" name="" id=""> --}}
-                  <i class='text-2xl text-gray-400 hover:text-blue-500 cursor-pointer bx bx-plus'></i>
+    <div class="md:mx-8">
+        <x-navbar></x-navbar>
+
+        {{-- TAMBAHIN INPUT PER ITEM, JADI BISA BELI LEBIH DARI 1 BARANG SEKALIGUS --}}
+        <h2 class="text-3xl font-bold text-gray-900 text-center mt-8">Keranjang</h2>
+        <div class="grid grid-cols-8 gap-x-8 mx-8 mt-4">
+            {{-- card section --}}
+            <div class="col-span-8 lg:col-span-5 flex flex-col">
+                <div class="flex flex-col space-y-8">
+                    @for ($i = 0; $i < 3; $i++)
+                        <div class="flex content-center items-center space-x-4 rounded-lg shadow-custom1 p-4">
+                            <input type="checkbox" name="" id="">
+                            <div class="cart-item-image-container rounded-lg"
+                                style="background-image: url({{ asset('images/IMG_7800.jpg') }}); background-size: cover; backround-repeat: none">
+                            </div>
+                            <div class="flex flex-col flex-grow h-full">
+                                <div>
+                                    <h4 class="text-lg font-semibold">Baju baru ni</h4>
+                                    <p class="">Rp. 2.000.000,00</p>
+                                </div>
+                                <div class="flex justify-end items-center mt-auto">
+                                    <p class="text-gray-400 hover:text-blue-500 cursor-pointer">Pindahkan ke Wishlist
+                                    </p>
+                                    <p class="text-gray-400">&nbsp;|&nbsp;</p>
+                                    <div class="flex flex-col align-center"><i
+                                            class='text-2xl text-gray-400 hover:text-blue-500 cursor-pointer bx bx-trash'></i>
+                                    </div>
+                                    <p class="text-gray-400">&nbsp;|&nbsp;</p>
+                                    <div class="flex space-x-2">
+                                        <i
+                                            class='text-2xl text-gray-400 hover:text-blue-500 cursor-pointer bx bx-minus'></i>
+                                        {{-- <input class="flex-shrink" type="number" name="" id=""> --}}
+                                        <i
+                                            class='text-2xl text-gray-400 hover:text-blue-500 cursor-pointer bx bx-plus'></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endfor
                 </div>
-              </div>
             </div>
-          </div>
-          @endfor
+            {{-- total price --}}
+            <div class="col-span-8 lg:col-span-3 mt-8 lg:mt-0">
+                <div class="rounded-lg shadow-custom1 p-4">
+                    <p class="font-semibold">Ringkasan Belanja</p>
+                    <hr class="my-4 border-gray-800">
+                    <div class="flex">
+                        <p class="text-gray-400">Total Harga</p>
+                        <p class="text-gray-400 ml-auto">Rp. xxx.xxx,xx</p>
+                    </div>
+                    <div class="flex">
+                        <p class="text-gray-400">Total Diskon</p>
+                        <p class="text-gray-400 ml-auto">Rp. xxx.xxx,xx</p>
+                    </div>
+                    <hr class="my-4 border-gray-800">
+                    <div class="flex">
+                        <p class="font-semibold">Total Belanja</p>
+                        <p class="font-semibold ml-auto">Rp. xxx.xxx,xx</p>
+                    </div>
+                    <button class="relative w-full bg-gray-800 hover:bg-opacity-90 rounded-lg p-2 mt-4">
+                        <a href="{{ route('checkout') }}">
+                            <p class="font-semibold text-white text-center">Checkout</p>
+                        </a>
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-      {{-- total price --}}
-      <div class="col-span-8 lg:col-span-3 mt-8 lg:mt-0">
-        <div class="rounded-lg shadow-custom1 p-4">
-          <p class="font-semibold">Ringkasan Belanja</p>
-          <hr class="my-4 border-gray-800">
-          <div class="flex">
-            <p class="text-gray-400">Total Harga</p>
-            <p class="text-gray-400 ml-auto">Rp. xxx.xxx,xx</p>
-          </div>
-          <div class="flex">
-            <p class="text-gray-400">Total Diskon</p>
-            <p class="text-gray-400 ml-auto">Rp. xxx.xxx,xx</p>
-          </div>
-          <hr class="my-4 border-gray-800">
-          <div class="flex">
-            <p class="font-semibold">Total Belanja</p>
-            <p class="font-semibold ml-auto">Rp. xxx.xxx,xx</p>
-          </div>
-          <button class="relative w-full bg-gray-800 hover:bg-opacity-90 rounded-lg p-2 mt-4">
-            <p class="font-semibold text-white text-center">Checkout</p>
-          </button>
-        </div>
-      </div>
     </div>
-  </div>
 </x-layout>
--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -1,122 +1,251 @@
 <x-layout titlePage="Hanaka | Cart">
-  <div class="md:mx-8">
-    <x-navbar></x-navbar>
+    <div class="md:mx-8">
+        <x-navbar></x-navbar>
 
-    <h2 class="text-3xl font-bold text-gray-900 text-center mt-8">Checkout Pesanan</h2>
-    <div class="grid grid-cols-8 gap-x-8 mx-8 mt-4 my-6">
-      {{-- form section  --}}
-      <div class="col-span-8 lg:col-span-5 shadow-custom1 px-5 py-4 rounded-lg">
-        <form action="" method="POST">
-            <div class="flex flex-col space-y-3">
-                <div class="flex flex-col space-y-1">
-                    <label for="nama" class="col-span-3">Nama Penerima</label>
-                    <input type="text"
-                        class="col-span-9 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                        placeholder="Michael Alexander" name="" id="nama" required maxlength="100">
-                </div>
-                <div class="flex flex-col space-y-1">
-                    <label for="alamat" class="col-span-3">Alamat</label>
-                    <input type="text"
-                        class="col-span-9 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                        placeholder="Jl. Paguyuban II No.14" name="" id="alamat" required maxlength="100">
-                </div>
-                <div class="flex flex-col space-y-1">
-                    <label for="provinsi">Provinsi</label>
-                    <select class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
-                        name="" id="provinsi">
-                        <option value="">Jawa Barat</option>
-                        <option value="">Jawa Timur</option>
-                        <option value="">Jawa Tengah</option>
-                    </select>
-                </div>
-                <div class="flex flex-col space-y-1">
-                    <label for="kota_kab">Kota/Kabupaten</label>
-                    <select class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
-                        name="" id="kota_kab">
-                        <option value="">Bekasi</option>
-                        <option value="">Gatau</option>
-                        <option value="">Asw</option>
-                    </select>
-                </div>
-                <div class="flex flex-col space-y-1">
-                    <label for="kecamatan">Kecamatan</label>
-                    <select class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
-                        name="" id="kecamatan">
-                        <option value="">Udah</option>
-                        <option value="">Gua</option>
-                        <option value="">Bilang,</option>
-                        <option value="">Gatau</option>
-                    </select>
-                </div>
-                <div class="grid grid-cols-12 gap-10">
-                    <div class="col-span-3 flex flex-col space-y-1">
-                        <label for="kode_pos" class="col-span-3">Kode Pos</label>
-                        <input type="text"
-                            class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                            placeholder="17115" name="" id="kode_pos" required maxlength="100">
-                    </div>
-                    <div class="col-span-9 flex flex-col space-y-1">
-                        <label for="no_telp" class="col-span-3">Nomor Telepon</label>
+        {{-- ======== PAGE IMPORTS ======== --}}
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+        <h2 class="text-3xl font-bold text-gray-900 text-center mt-8">Checkout Pesanan</h2>
+        <div class="grid grid-cols-8 gap-x-8 mx-8 mt-4 my-6">
+            {{-- form section --}}
+            <div class="col-span-8 lg:col-span-5 shadow-custom1 px-5 py-4 rounded-lg">
+                <form action="" method="POST">
+                    <div class="flex flex-col space-y-3">
+                        <div class="flex flex-col space-y-1">
+                            <label for="nama" class="col-span-3">Nama Penerima</label>
+                            <input type="text"
+                                class="col-span-9 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                placeholder="Michael Alexander" name="" id="nama" required maxlength="100">
+                        </div>
+                        <div class="flex flex-col space-y-1">
+                            <label for="alamat" class="col-span-3">Alamat</label>
+                            <input type="text"
+                                class="col-span-9 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                placeholder="Jl. Paguyuban II No.14" name="" id="alamat" required maxlength="100">
+                        </div>
+                        <div class="flex flex-col space-y-1">
+                            <label for="provinsi">Provinsi</label>
+                            <select class="select2-data-array browser-default" id="select2-provinsi"></select>
+                        </div>
+                        <div class="flex flex-col space-y-1">
+                            <label for="kota_kab">Kota/Kabupaten</label>
+                            <select class="select2-data-array browser-default" id="select2-kabupaten"></select>
+                        </div>
+                        <div class="flex flex-col space-y-1">
+                            <label for="kecamatan">Kecamatan</label>
+                            <select class="select2-data-array browser-default" id="select2-kecamatan"></select>
+                        </div>
+                        <div class="grid grid-cols-12 gap-10">
+                            <div class="col-span-3 flex flex-col space-y-1">
+                                <label for="kode_pos" class="col-span-3">Kode Pos</label>
+                                <input type="text"
+                                    class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                    placeholder="17115" name="" id="kode_pos" required maxlength="100">
+                            </div>
+                            <div class="col-span-9 flex flex-col space-y-1">
+                                <label for="no_telp" class="col-span-3">Nomor Telepon</label>
+                                <div class="flex">
+                                    <span
+                                        class="flex items-center bg-grey-lighter rounded rounded-r-none border border-r-0 border-gray-300 px-3 text-grey-dark text-sm">+62</span>
+                                    <input type="text"
+                                        class="w-full border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        placeholder="81234567891" name="" id="no_telp" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col space-y-1">
+                            <label for="metode">Metode Pembayaran</label>
+                            <select
+                                class="select2-basic w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                name="" id="metode">
+                                <option value="">Transfer Virtual Account</option>
+                                <option value="">Transfer Bank</option>
+                            </select>
+                        </div>
+                        <div class="flex flex-col space-y-1">
+                            <label for="metode">Catatan Pemesanan (opsional)</label>
+                            <textarea
+                                class="col-span-9 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                name="" id="" cols="30" rows="5" style="resize: none"></textarea>
+                        </div>
                         <div class="flex">
-                            <span class="flex items-center bg-grey-lighter rounded rounded-r-none border border-r-0 border-gray-300 px-3 text-grey-dark text-sm">+62</span>
-                            <input type="text" 
-                                class="w-full border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                placeholder="81234567891" name="" id="no_telp" required>
+                            <button
+                                class="relative w-full bg-gray-800 hover:bg-opacity-90 rounded-lg p-2 mt-4 font-semibold text-white text-center">
+                                Bayar
+                            </button>
                         </div>
                     </div>
-                </div>
-                <div class="flex flex-col space-y-1">
-                    <label for="metode">Metode Pembayaran</label>
-                    <select class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
-                        name="" id="metode">
-                        <option value="">Transfer Virtual Account</option>
-                        <option value="">Transfer Bank</option>
-                    </select>
-                </div>
-                <div class="flex flex-col space-y-1">
-                    <label for="metode">Catatan Pemesanan (opsional)</label>
-                    <textarea class="col-span-9 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
-                        name="" id="" cols="30" rows="5" style="resize: none"></textarea>
-                </div>
-                <div class="flex">
-                    <button class="relative w-full bg-gray-800 hover:bg-opacity-90 rounded-lg p-2 mt-4 font-semibold text-white text-center">
-                        Bayar
-                    </button>
+                </form>
+            </div>
+            {{-- total price --}}
+            <div class="col-span-8 lg:col-span-3 mt-8 lg:mt-0">
+                <div class="rounded-lg shadow-custom1 p-4">
+                    <p class="font-semibold">Detail Pesanan</p>
+                    <hr class="mt-3 mb-2 border-gray-800">
+                    <div class="flex bg-gray-300 py-1 font-bold">
+                        <p class="">Nama Produk</p>
+                        <p class="ml-auto">Subtotal</p>
+                    </div>
+                    @for ($i = 0; $i < 2; $i++)
+                        <div class="flex">
+                            <p class="text-gray-400">Varsity Jacket Army x 2</p>
+                            <p class="text-gray-400 ml-auto">Rp.790.000</p>
+                        </div>
+                    @endfor
+                    <hr class="my-2 border-gray-800">
+                    <div class="flex font-bold">
+                        <p class="">Subtotal Produk</p>
+                        <p class="ml-auto">Rp.965.000</p>
+                    </div>
+                    <div class="flex font-bold">
+                        <p class="">Ongkos Kirim</p>
+                        <p class="ml-auto">Rp.10.000</p>
+                    </div>
+                    <hr class="my-2 border-gray-800">
+                    <div class="flex font-bold">
+                        <p class="">Total</p>
+                        <p class="ml-auto">Rp.965.000</p>
+                    </div>
                 </div>
             </div>
-        </form>
-      </div>
-      {{-- total price --}}
-      <div class="col-span-8 lg:col-span-3 mt-8 lg:mt-0">
-        <div class="rounded-lg shadow-custom1 p-4">
-          <p class="font-semibold">Detail Pesanan</p>
-          <hr class="mt-3 mb-2 border-gray-800">
-          <div class="flex bg-gray-300 py-1 font-bold">
-            <p class="">Nama Produk</p>
-            <p class="ml-auto">Subtotal</p>
-          </div>
-          @for ($i = 0; $i < 2; $i++)
-            <div class="flex">
-                <p class="text-gray-400">Varsity Jacket Army x 2</p>
-                <p class="text-gray-400 ml-auto">Rp.790.000</p>
-            </div>
-          @endfor
-          <hr class="my-2 border-gray-800">
-          <div class="flex font-bold">
-            <p class="">Subtotal Produk</p>
-            <p class="ml-auto">Rp.965.000</p>
-          </div>
-          <div class="flex font-bold">
-            <p class="">Ongkos Kirim</p>
-            <p class="ml-auto">Rp.10.000</p>
-          </div>
-          <hr class="my-2 border-gray-800">
-          <div class="flex font-bold">
-            <p class="">Total</p>
-            <p class="ml-auto">Rp.965.000</p>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
+    <script>
+        $(document).ready(function() {
+            $('.select2-basic').select2({
+                minimumResultsForSearch: Infinity
+            });
+        });
+
+        let urlProvinsi = "https://ibnux.github.io/data-indonesia/provinsi.json";
+        let urlKabupaten = "https://ibnux.github.io/data-indonesia/kabupaten/";
+        let urlKecamatan = "https://ibnux.github.io/data-indonesia/kecamatan/";
+        let urlKelurahan = "https://ibnux.github.io/data-indonesia/kelurahan/";
+
+        function clearOptions(id) {
+            console.log("on clearOptions");
+
+            //$('#' + id).val(null);
+            $('#' + id).empty().trigger('change');
+        }
+
+        console.log('Load Provinsi...');
+        $.getJSON(urlProvinsi, function(res) {
+
+            res = $.map(res, function(obj) {
+                obj.text = obj.nama
+                return obj;
+            });
+
+            data = [{
+                id: "",
+                nama: "- Pilih Provinsi -",
+                text: "- Pilih Provinsi -"
+            }].concat(res);
+
+            //implemen data ke select provinsi
+            $("#select2-provinsi").select2({
+                dropdownAutoWidth: true,
+                width: '100%',
+                data: data
+            })
+        });
+
+        let selectProv = $('#select2-provinsi');
+        $(selectProv).change(function() {
+            console.log("on change selectProv");
+
+            let value = $(selectProv).val();
+            let text = $('#select2-provinsi :selected').text();
+            console.log("value = " + value + " / " + "text = " + text);
+
+            clearOptions('select2-kabupaten');
+            console.log('Load Kabupaten di ' + text + '...')
+            $.getJSON(urlKabupaten + value + ".json", function(res) {
+
+                res = $.map(res, function(obj) {
+                    obj.text = obj.nama
+                    return obj;
+                });
+
+                data = [{
+                    id: "",
+                    nama: "- Pilih Kabupaten -",
+                    text: "- Pilih Kabupaten -"
+                }].concat(res);
+
+                //implemen data ke select provinsi
+                $("#select2-kabupaten").select2({
+                    dropdownAutoWidth: true,
+                    width: '100%',
+                    data: data
+                })
+            })
+        });
+
+        let selectKab = $('#select2-kabupaten');
+        $(selectKab).change(function() {
+            console.log("on change selectKab");
+
+            let value = $(selectKab).val();
+            let text = $('#select2-kabupaten :selected').text();
+            console.log("value = " + value + " / " + "text = " + text);
+
+            clearOptions('select2-kecamatan');
+            console.log('Load Kecamatan di ' + text + '...')
+            $.getJSON(urlKecamatan + value + ".json", function(res) {
+
+                res = $.map(res, function(obj) {
+                    obj.text = obj.nama
+                    return obj;
+                });
+
+                data = [{
+                    id: "",
+                    nama: "- Pilih Kecamatan -",
+                    text: "- Pilih Kecamatan -"
+                }].concat(res);
+
+                //implemen data ke select provinsi
+                $("#select2-kecamatan").select2({
+                    dropdownAutoWidth: true,
+                    width: '100%',
+                    data: data
+                })
+            })
+        });
+
+        let selectKec = $('#select2-kecamatan');
+        $(selectKec).change(function() {
+            console.log("on change selectKec");
+
+            let value = $(selectKec).val();
+            let text = $('#select2-kecamatan :selected').text();
+            console.log("value = " + value + " / " + "text = " + text);
+
+            clearOptions('select2-kelurahan');
+            console.log('Load Kelurahan di ' + text + '...')
+            $.getJSON(urlKelurahan + value + ".json", function(res) {
+
+                res = $.map(res, function(obj) {
+                    obj.text = obj.nama
+                    return obj;
+                });
+
+                data = [{
+                    id: "",
+                    nama: "- Pilih Kelurahan -",
+                    text: "- Pilih Kelurahan -"
+                }].concat(res);
+
+                //implemen data ke select provinsi
+                $("#select2-kelurahan").select2({
+                    dropdownAutoWidth: true,
+                    width: '100%',
+                    data: data
+                })
+            })
+        });
+    </script>
 </x-layout>
